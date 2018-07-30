3674fc0260424b2ccc1cdc8898b4b3eb4349e506
Noah Zani <noah.zani@outlook.com>
2018-07-30 10:13:29 +0200

    QnA-Bot & Toggle
    chatbot is integrated and features can be toggled via the navbar

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2375,3 +2375,21 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+
+// custom code
+// chatbot integration
+
+(function () {
+	var div = document.createElement("div");
+	document.getElementsByTagName('body')[0].appendChild(div);
+	div.outerHTML = "<div id='botDiv' style='height: 38px; position: fixed; bottom: 0; z-index: 1000; background-color: #fff'><div id='botTitleBar' style='height: 38px; width: 400px; position:fixed; cursor: pointer;'></div><iframe width='400px' height='600px' src='https://webchat.botframework.com/embed/qnabot-test-noah?s=JpzkNCt2AGQ.cwA.f4A.UfmCI5f12zdeeDpWJlUnMWrW906QQVGhICl-Nmk3Lww'></iframe></div>"; 
+
+	document.querySelector('body').addEventListener('click', function (e) {
+			e.target.matches = e.target.matches || e.target.msMatchesSelector;
+			if (e.target.matches('#botTitleBar')) { 
+					var botDiv = document.querySelector('#botDiv'); 
+					botDiv.style.height = botDiv.style.height == '600px' ? '38px' : '600px';
+			};
+	});
+}());
